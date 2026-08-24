e4bdaa5faddb4a93de7cbacddfcceb6af2d5be05
sakin111 <maliksakin53@gmail.com>
2026-08-24 15:02:21 +0600

    feat: add prisma.config.ts for Prisma configuration

---
+++ prisma.config.ts
@@ -0,0 +1,13 @@
+﻿import { defineConfig } from 'prisma/config';
+
+export default defineConfig({
+  schema: 'prisma/schema',
+
+  migrations: {
+    path: 'prisma/migrations',
+  },
+
+  datasource: {
+    url: process.env.DATABASE_URL! || process.env.DIRECT_URL!,
+  },
+});
